72463be09b17613fc7a3b8c260d7e7e43a956ea1
misha-jiltsov <misha.jiltsov@gmail.com>
2024-03-04 23:40:32 +0000

    added timer in bottom right

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import random
 BACKGROUND_BLUE = (20, 89, 199)
 
 pygame.init()
+pygame.font.init()
+gamefont = pygame.font.SysFont("Comic Sans MS", 30)
 screen_width, screen_height = 500, 500
 screen = pygame.display.set_mode([screen_width, screen_height])
 
@@ -17,17 +19,18 @@ obstacle_list = pygame.sprite.Group()
 player = Player(200, 200)
 player_list.add(player)
 
-
+def join(a, b):
+    return str(a)+str(b)
 
 done = False
-
+timerunning = 0
 clock = pygame.time.Clock()
 
 def scatter():
     while len(obstacle_list)!=9:
 
-        randomvel1 = random.choice([(random.randint(75, 100)/100+0.03), (random.randint(-100, -75)/100-0.03)])
-        randomvel2 = random.choice([(random.randint(75, 100) / 100 + 0.03), (random.randint(-100, -75) / 100 - 0.03)])
+        randomvel1 = random.choice([(random.randint(50, 150)/100+0.03), (random.randint(-150, -50)/100-0.03)])
+        randomvel2 = random.choice([(random.randint(50, 150) / 100 + 0.03), (random.randint(-150, -50) / 100 - 0.03)])
 
         randomposx = random.choice([random.randint(300, 400), random.randint(20, 100)])
         randomposy = random.choice([random.randint(300, 400), random.randint(20, 100)])
@@ -76,6 +79,11 @@ while not done:
             player_list.remove(playerobject)
             done = True
 
+    timerunning+=(1/60)
+
+    timedisplay = f"{int(timerunning//60)}:{int(timerunning%60) if len(str(int(timerunning%60))) == 2 else join('0', int(timerunning%60))}"
+    textsurface = gamefont.render(timedisplay, False, (0, 0, 0))
+    screen.blit(textsurface, (400, 450))
 
     clock.tick(60)
     pygame.display.flip()
