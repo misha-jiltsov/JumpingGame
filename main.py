85505f989e78e4898862912232608b2af39f5ec2
misha-jiltsov <misha.jiltsov@gmail.com>
2024-03-04 23:56:44 +0000

    added wall collision death

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sprites import Player, Obstacle
 import random
 
 name = input("Enter your name")
-
+gameactive = False
 BACKGROUND_BLUE = (20, 89, 199)
 
 pygame.init()
@@ -43,51 +43,63 @@ def scatter():
         obstacle_list.add(newObject)
 
 while not done:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    if len(obstacle_list) == 0:
-        scatter()
-
-    collisionlist = pygame.sprite.spritecollide(player, obstacle_list, False)
-
-
-
-    screen.fill(BACKGROUND_BLUE)
-    player_list.draw(screen)
-    obstacle_list.draw(screen)
-
     keys = pygame.key.get_pressed()
 
-    if keys[K_a] or keys[K_LEFT]:
-        player.setvelocityx(-2)
-    elif keys[K_d] or keys[K_RIGHT]:
-        player.setvelocityx(2)
-    else:
-        player.setvelocityx(0)
-    if keys[K_SPACE]:
-        player.jump()
+    if not gameactive:
+        if keys[K_SPACE]:
+            gameactive = True
+
+    if gameactive:
+        if len(obstacle_list) == 0:
+            scatter()
+
+        collisionlist = pygame.sprite.spritecollide(player, obstacle_list, False)
 
 
-    for playerobject in player_list:
-        playerobject.updateposition()
 
-    for obs in obstacle_list:
-        obs.updateself()
+        screen.fill(BACKGROUND_BLUE)
+        player_list.draw(screen)
+        obstacle_list.draw(screen)
 
-    ########### do kill
+        keys = pygame.key.get_pressed()
+        if keys[K_a] or keys[K_LEFT]:
+            player.setvelocityx(-2)
+        elif keys[K_d] or keys[K_RIGHT]:
+            player.setvelocityx(2)
+        else:
+            player.setvelocityx(0)
+        if keys[K_SPACE]:
+            player.jump()
 
-    if len(collisionlist)>0:
+
         for playerobject in player_list:
-            player_list.remove(playerobject)
-            done = True
+            playerobject.updateposition()
 
-    timerunning+=(1/60)
+        for obs in obstacle_list:
+            obs.updateself()
 
-    timedisplay = f"{int(timerunning//60)}:{int(timerunning%60) if len(str(int(timerunning%60))) == 2 else join('0', int(timerunning%60))}"
-    textsurface = gamefont.render(timedisplay, False, (0, 0, 0))
-    screen.blit(textsurface, (400, 450))
+        ########### do kill
+
+        if len(collisionlist)>0:
+            for playerobject in player_list:
+                player_list.remove(playerobject)
+                done = True
+
+        if player.getpos()[0]>500 or player.getpos()[0]<0 or player.getpos()[1]>500 or player.getpos()[1]<0:
+            for playerobject in player_list:
+                player_list.remove(playerobject)
+                done = True
+
+        timerunning+=(1/60)
+
+        timedisplay = f"{int(timerunning//60)}:{int(timerunning%60) if len(str(int(timerunning%60))) == 2 else join('0', int(timerunning%60))}"
+        textsurface = gamefont.render(timedisplay, False, (0, 0, 0))
+        screen.blit(textsurface, (400, 450))
 
     clock.tick(60)
     pygame.display.flip()
@@ -95,8 +107,6 @@ while not done:
 
 with open("all_scores.csv", "a") as file:
     file.write(f"{timedisplay},{name}")
-
-
 
 
 pygame.quit()
